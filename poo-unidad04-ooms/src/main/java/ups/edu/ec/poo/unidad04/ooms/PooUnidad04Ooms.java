9baa7906aca691caa7f705d8158fca50904ea92f
mortizo <mortizo@ups.edu.ec>
2022-05-12 23:28:18 -0500

    Unidad 04 inicial

---
+++ poo-unidad04-ooms/src/main/java/ups/edu/ec/poo/unidad04/ooms/PooUnidad04Ooms.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ups.edu.ec.poo.unidad04.ooms;
+
+/**
+ *
+ * @author morti
+ */
+public class PooUnidad04Ooms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
