7e77ad113fc0e3abc2b4ddb93177f7834ad15224
UPS <UPS@DESKTOP-8FGL23A>
2023-02-16 21:55:42 -0500

    Cambio MatematicaTest

--- poo-unidad04-ooms/src/test/java/p61/u04_03/MatematicaTest.java
+++ poo-unidad04-ooms/src/test/java/p61/u04_03/MatematicaTest.java
@@ -48,6 +48,8 @@ public class MatematicaTest {
         assertEquals(expResult, result, 0);
         // TODO review the generated test code and remove the default call to fail.
       //  fail("The test case is a prototype.");
+      
+      
     }
 
     /**
